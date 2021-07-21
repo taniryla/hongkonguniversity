4acbf8b24a780ae9749daf99b451c014db257f9e
taniryla <ryland0000@gmail.com>
2021-07-20 20:05:21 -0600

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Reistorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
-}
+
+import Menu from './components/MenuComponent';
+
+
+
+    <Menu />
 
 export default App;
